1a3ffa266763f129ad969f4fbca30e9a2fd313fb
athirapaul <athirapaul24@gmail.com>
2019-02-08 18:11:41 -0500

    Initial Commit

---
+++ newproject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  newproject
+//
+//  Created by Athira Paul on 2019-02-08.
+//  Copyright © 2019 Athira Paul. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
